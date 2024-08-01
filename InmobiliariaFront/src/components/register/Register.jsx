377444e0475a71c3df476679638ecae20fa32c85
pedrovergaraa <pvergara2407@gmail.com>
2024-08-01 11:01:19 -0300

    Login y Register hechos

--- InmobiliariaFront/src/components/register/Register.jsx
+++ InmobiliariaFront/src/components/register/Register.jsx
@@ -1,9 +1,8 @@
 // src/components/Login.jsx
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import './Login.css'; 
 
-function Login() {
+function Register() {
+  const [email, setEmail] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
@@ -16,24 +15,45 @@ function Login() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(`Username: ${username}, Password: ${password}`);
+    console.log(`Username: ${username}, Email: ${email}, Password: ${password}`);
   };
 console.log("username", username)
+console.log("email", email)
 console.log("password", password)
   return (
     <div className="login-container">
       <div className="card login-card">
         <div className="card-body">
-          <h2 className="card-title">Login</h2>
+          <h2 className="card-title">Register</h2>
           <form onSubmit={handleSubmit}>
             <div className="form-group">
               <label htmlFor="username">Username:</label>
+              <input
+                type="username"
+                id="username"
+                className="form-control"
+                value={username}
+                onChange={(e) => handleInputChange(e, setUsername)}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="username">Repeat Username:</label>
               <input
                 type="name"
                 id="name"
                 className="form-control"
                 value={username}
                 onChange={(e) => handleInputChange(e, setUsername)}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                id="email"
+                className="form-control"
+                value={email}
+                onChange={(e) => handleInputChange(e, setEmail)}
               />
             </div>
             <div className="form-group">
@@ -46,7 +66,6 @@ console.log("password", password)
                 onChange={(e) =>handleInputChange(e, setPassword)}
               />
             </div>
-            <p>Olvidaste tu contraseña? <Link to="/register" className="btn btn-secondary">Register</Link></p>
             <button type="submit" className="btn btn-primary btn-block">Iniciar sesion</button>
           </form>
         </div>
@@ -55,4 +74,4 @@ console.log("password", password)
   );
 }
 
-export default Login;
+export default Register;
--- InmobiliariaFront/src/routes/index.jsx
+++ InmobiliariaFront/src/routes/index.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from '../components/login';
+import Register from '../components/register/Register';
 
 function HandleRoutes() {
   return (
     <Router>
       <Routes>
+        <Route path="/register" element={<Register/>} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
