9e06696a029f7f7cfa436aa84f6be01ed5cbec9f
marti <marti@LAPTOP-3QT7R1C7>
2024-08-01 18:39:23 -0300

    agregue el navbar y lleva a register y a login

--- InmobiliariaFront/src/routes/index.jsx
+++ InmobiliariaFront/src/routes/index.jsx
@@ -1,15 +1,19 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from '../components/login';
 import Register from '../components/register/Register';
+import Navbar from '../components/navbar/Navbar';
 
 function HandleRoutes() {
   return (
+    <>  
     <Router>
+    <Navbar></Navbar>
       <Routes>
         <Route path="/register" element={<Register/>} />
         <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
+    </>
   );
 }
 
