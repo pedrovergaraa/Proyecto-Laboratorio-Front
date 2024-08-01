7b7f7cbe0f22ae8a736d25ce68b44e03bd2b647d
Pedro Vergara <pvergara2407@gmail.com>
2024-08-01 17:59:23 -0300

    Prueba conexion con Back

--- InmobiliariaFront/src/routes/index.jsx
+++ InmobiliariaFront/src/routes/index.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from '../components/login';
 import Register from '../components/register/Register';
+import Users from '../components/users/Users';
 
 function HandleRoutes() {
   return (
     <Router>
       <Routes>
+        <Route path="/users" element={<Users />} />
         <Route path="/register" element={<Register/>} />
         <Route path="/login" element={<Login />} />
       </Routes>
