c6b340ff91a4bece4ccbe2af14dc851154a53cab
pedrovergaraa <pvergara2407@gmail.com>
2024-09-17 11:19:41 -0300

    Cambio icono themeContext light/dark

--- InmobiliariaFront/src/components/ui/toggleTheme/ToggleTheme.jsx
+++ InmobiliariaFront/src/components/ui/toggleTheme/ToggleTheme.jsx
@@ -2,14 +2,20 @@ import { useContext } from "react";
 import { Button } from "react-bootstrap";
 import { ThemeContext } from "../../../services/themeContext/theme.context";
 import ModeNightOutlinedIcon from '@mui/icons-material/ModeNightOutlined';
+import WbSunnyOutlinedIcon from '@mui/icons-material/WbSunnyOutlined';
 
 const ToggleTheme = ({ className }) => {
     const { toggleTheme, theme } = useContext(ThemeContext);
+
     return (
-        <Button className={className} onClick={toggleTheme} startIcon={<ModeNightOutlinedIcon/>}>  <ModeNightOutlinedIcon style={{ marginRight: '8px' }} />
-            {theme === "light"} {theme === "light"}</Button>
-            // {theme === "light" ? "oscuro" : "claro"} {theme === "light" ? "oscuro" : "claro"}</Button>
-        );
+        <Button className={`theme-toggle-button ${className}`} onClick={toggleTheme}>
+            {theme === "light" ? (
+                <ModeNightOutlinedIcon />
+            ) : (
+                <WbSunnyOutlinedIcon />
+            )}
+        </Button>
+    );
 };
 
 export default ToggleTheme;
